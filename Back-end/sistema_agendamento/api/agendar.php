379e2332c91875dcb414efeeb0371a1e71b2b5c4
Israel Paz <israelnetmax@gmail.com>
2025-05-08 16:57:16 -0300

    organizando o front

--- Back-end/sistema_agendamento/api/agendar.php
+++ Back-end/sistema_agendamento/api/agendar.php
@@ -17,3 +17,22 @@ if (isset($data["nome"]) && isset($data["data"])) {
     echo json_encode(["error" => "Dados incompletos."]);
 }
 ?>
+<?php
+require_once "../config/database.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (isset($data["nome"]) && isset($data["data"])) {
+    $stmt = $pdo->prepare("INSERT INTO agendamentos (nome, data) VALUES (:nome, :data)");
+    $stmt->bindParam(":nome", $data["nome"]);
+    $stmt->bindParam(":data", $data["data"]);
+    
+    if ($stmt->execute()) {
+        echo json_encode(["message" => "Agendamento criado com sucesso!"]);
+    } else {
+        echo json_encode(["error" => "Erro ao agendar."]);
+    }
+} else {
+    echo json_encode(["error" => "Dados incompletos."]);
+}
+?>
